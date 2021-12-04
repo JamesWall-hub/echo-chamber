2e07aaefb8556e409d87384b1c77901fb7e6a8a5
JamesWall-hub <james.sean.wall@gmail.com>
2021-12-04 11:23:43 +0000

    moves loading message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,20 +19,23 @@ const Home = () => {
         })
     }, [])
     return(
-        isLoading ? <p>Loading...</p> :
-        isError ? <p>Something went wrong!</p> :
         <div className="Home">
-        <h2>Most popular:</h2>
-            <ul className="ArticleList" style={{listStyleType: "none"}}>
-            {popularArticles.map((article) => {
-                const {article_id, title, author, topic, votes, comment_count, created_at} = article
-                return(
+            {isLoading ? <p>Loading...</p> :
+            isError ? <p>Something went wrong!</p> :
+            <>
+                <h2>Most popular:</h2>
+                <ul className="ArticleList" style={{listStyleType: "none"}}>
+                    {popularArticles.map((article) => {
+                    const {article_id, title, author, topic, votes, comment_count, created_at} = article
+                    return(
                     <li key={article_id}>
                         <ArticleCard article_id={article_id} title={title} author={author} topic={topic} votes={votes} comment_count={comment_count} created_at={created_at}/>
                     </li>
-                )
-            })}
-            </ul>
+                    )
+                    })}
+                </ul>
+            </>
+            }          
         </div>
     )
 }
